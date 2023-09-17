a271f96336d376e568da9e05be4e4902e1f175dc
beshoynady <bishoin2017@gmail.com>
2023-09-18 01:38:33 +0300

    add github link for image

--- client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
+++ client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
@@ -14,7 +14,7 @@ const MenuCard = () => {
                      {allProducts.filter(pro => pro.category === categoryid).map((product, index) => {
                         return (
                            <div className="menu-card" key={index}>
-                              <img className='img-card' src={`../images/${product.image}`} alt="" />
+                              <img className='img-card' src={`https://raw.githubusercontent.com/beshoynady/restaurant-api/main/server/images/${product.image}`} alt="" />
                               {product._id==productid&noteArea==true?<form onSubmit={(e)=>{addnotrstoproduct(e,product._id);;setnoteArea(!noteArea)}}>
                                     <textarea name="note" cols="100" rows="3" onChange={(e)=>{setproductnote(e.target.value)}}></textarea>
                                     <div className='note-btn'>
