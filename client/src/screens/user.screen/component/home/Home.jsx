f1aadc7acf78eb4d176a30cbf6b30d9d64372609
beshoynady <bishoin2017@gmail.com>
2023-09-19 03:42:36 +0300

    f

--- client/src/screens/user.screen/component/home/Home.jsx
+++ client/src/screens/user.screen/component/home/Home.jsx
@@ -13,13 +13,16 @@ const Home = () => {
             <main className='main-home'>
               <div className="container">
                 <div className="content">
-                  {userlogininfo?<p className='main-title'>مرحبا {usertitle(userlogininfo._id)} في</p>:id?<p className='main-title'>مرحبا ضيوف {usertitle(id)} في</p>:<p className='main-title'>مرحبا بكم في</p>
+                  {userlogininfo?<p className='main-title'>مرحبا {usertitle(userlogininfo.id)} في</p>
+                  :id?<p className='main-title'>مرحبا ضيوف {usertitle(id)} في</p>:<p className='main-title'>مرحبا بكم في</p>
                   }
                   <p className='main-text'>كافيار للمأكولات البحرية <br />Caviar Seafood</p>
                   <ul className="main-btn">
 
-                    {id?<><li className='main-li' onClick={()=>askingForHelp(id)}><a>طلب الويتر</a></li>
-                    <li className='main-li'><a href="#menu">المنيو</a></li></>
+                    {id?<>
+                    <li className='main-li' onClick={()=>askingForHelp(id)}><a>طلب الويتر</a></li>
+                    <li className='main-li'><a href="#menu">المنيو</a></li>
+                    </>
                     :<li className='main-li'><a href="#menu">المنيو</a></li>}
                   </ul>
                 </div>
