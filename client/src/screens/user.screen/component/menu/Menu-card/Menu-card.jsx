f8946d1e799f0941b2cdae756aa07286d53fabb1
beshoynady <bishoin2017@gmail.com>
2023-09-22 02:30:39 +0300

    btn

--- client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
+++ client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
@@ -41,7 +41,7 @@ const MenuCard = () => {
                                     </div>
                                  </div>
                                  <div className='card-btn'>
-                                 {itemid.filter((i)=>i == product._id).length>0?
+                                 {itemid.filter((i)=>i == product._id).length>0 && product.quantity>0?
                               <button className='delfromcart' onClick={() => { deleteitems(product._id);setitemid(itemid.filter((i)=> i !== product._id)) }}>احذف من الطلبات</button>
                               : <button className='addtocart' onClick={() => { if (product.quantity > 0) { additemtocart(product._id, product.quantity) };setitemid([...itemid , product._id]) }}>اضف الي طلباتي</button>}
                                  </div>
