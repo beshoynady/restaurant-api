010729b8acb77ec2707d1bc7327ee65440bad488
beshoynady <bishoin2017@gmail.com>
2023-09-21 18:06:26 +0300

    u-offers

--- client/src/screens/user.screen/component/offers/Offers.jsx
+++ client/src/screens/user.screen/component/offers/Offers.jsx
@@ -1,4 +1,6 @@
 import React, { useRef, useState } from 'react';
+import { detacontext } from '../../../../App'
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -13,52 +15,109 @@ import './Offers.css';
 import { EffectCoverflow, Pagination } from 'swiper/modules';
 
 export default function Offers() {
+  const [noteArea, setnoteArea] = useState(false)
+  const [productid, setproductid] = useState('')
   return (
-    <>
-      <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-        </SwiperSlide>
-      </Swiper>
-    </>
+    <detacontext.Consumer>
+      {
+        ({ allProducts, categoryid, additemtocart, increment, descrement, setproductnote, addnotrstoproduct, }) => {
+          return (
+            <section className='offers-section'>
+              <div className='section-title'>
+                <h2>OFFERS</h2>
+              </div>
+              <Swiper
+                effect={'coverflow'}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={'auto'}
+                coverflowEffect={{
+                  rotate: 50,
+                  stretch: 0,
+                  depth: 100,
+                  modifier: 1,
+                  slideShadows: true,
+                }}
+                pagination={true}
+                modules={[EffectCoverflow, Pagination]}
+                className="mySwiper"
+              >
+                {allProducts.filter(pro => pro.category === categoryid).map((product, index) => {
+                  return (
+                    <SwiperSlide>
+                      <div className="offer-card" key={index}>
+                        <img className='offer-img' src={`https://raw.githubusercontent.com/beshoynady/restaurant-api/main/server/images/${product.image}`} alt="" />
+                        {product._id == productid & noteArea == true ?
+                          <div className='offers-note'>
+                            <form onSubmit={(e) => { addnotrstoproduct(e, product._id);; setnoteArea(!noteArea) }}>
+                              <textarea name="note" cols="100" rows="3" onChange={(e) => { setproductnote(e.target.value) }}></textarea>
+                              <div className='note-btn'>
+                                <button>تاكيد</button>
+                                <button onClick={() => setnoteArea(!noteArea)}>الغاء</button>
+                              </div>
+                            </form>
+                          </div>
+                          : ''}
+
+                        <div className="offer-detalis">
+                          <div className='offer-info'>
+                            <div className='product-name'>
+                              <h2>{product.name}</h2>
+                              <span className="material-symbols-outlined" onClick={() => { setnoteArea(!noteArea); setproductid(product._id) }}>note_alt</span>
+                            </div>
+                            <p>{product.description}</p>
+                          </div>
+                          <div className="offer-price">
+                            <div className='p-price'>{product.price}ج</div>
+                            <div className="p-counter">
+                              <button className='counter-symb' onClick={() => descrement(product._id)}>-</button>
+                              <div className='counter-num'>{product.quantity}</div>
+                              <button className='counter-symb' onClick={() => increment(product._id)}>+</button>
+                            </div>
+                          </div>
+                          <div className='offer-card-btn'>
+                            <button className='addtocart' onClick={() => { if (product.quantity > 0) { additemtocart(product._id, product.quantity) } }}>اضف الي طلباتي</button>
+                          </div>
+                        </div>
+                      </div>
+
+                    </SwiperSlide>
+                  )
+                }
+                )}
+                {/* <SwiperSlide>
+                  <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+                </SwiperSlide> */}
+              </Swiper>
+            </section>
+          )
+        }
+      }
+    </detacontext.Consumer>
   );
+
 }
