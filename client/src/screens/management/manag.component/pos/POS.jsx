b1bea176b71678526939ebf60285daad296179ec
beshoynady <bishoin2017@gmail.com>
2023-09-22 15:27:07 +0300

    pos menu

--- client/src/screens/management/manag.component/pos/POS.jsx
+++ client/src/screens/management/manag.component/pos/POS.jsx
@@ -45,16 +45,16 @@ const POS = () => {
                 {allProducts.filter(pro => pro.category === categoryid).map((product, index) => {
                   return (
                     <div className="pos-card" key={index}>
-                      {/* <img className='img-card' src={`https://raw.githubusercontent.com/beshoynady/restaurant-api/main/server/images/${product.image}`} alt="" /> */}
-                      {/* {product._id == productid & noteArea == true ? <form onSubmit={(e) => { addnotrstoproduct(e, product._id);; setnoteArea(!noteArea) }}>
+                      <img className='pos-img-card' src={`https://raw.githubusercontent.com/beshoynady/restaurant-api/main/server/images/${product.image}`} alt="" />
+                      {product._id == productid & noteArea == true ? <form onSubmit={(e) => { addnotrstoproduct(e, product._id);; setnoteArea(!noteArea) }}>
                         <textarea placeholder='اضف تعليماتك الخاصة بهذا الطبق' name="note" cols="100" rows="3" onChange={(e) => { setproductnote(e.target.value) }}></textarea>
                         <div className='note-btn'>
                           <button>تاكيد</button>
                           <button onClick={() => setnoteArea(!noteArea)}>الغاء</button>
                         </div>
-                      </form> : ''} */}
+                      </form> : ''}
 
-                      <div className="detalis">
+                      <div className="pos-card-detalis">
                         <div className='product-det'>
                           <div className='product-name'>
                             <h2>{product.name}</h2>
