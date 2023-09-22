7cac0374d697b7167cb5b41884afc1d2f267f167
beshoynady <bishoin2017@gmail.com>
2023-09-22 16:02:38 +0300

    u pos card

--- client/src/screens/management/manag.component/pos/POS.jsx
+++ client/src/screens/management/manag.component/pos/POS.jsx
@@ -16,6 +16,8 @@ const POS = () => {
   });
 
   const { id } = useParams()
+
+  const [itemid, setitemid] = useState([])
   const [noteArea, setnoteArea] = useState(false)
   const [productid, setproductid] = useState('')
 
@@ -55,27 +57,28 @@ const POS = () => {
                       </form> : ''}
 
                       <div className="pos-card-detalis">
-                        <div className='product-det'>
-                          <div className='product-name'>
-                            <h2>{product.name}</h2>
-                            <span className="material-symbols-outlined" onClick={() => { setnoteArea(!noteArea); setproductid(product._id) }}>note_alt</span>
+                        {/* <div className='pos-card-head'> */}
+                          <div className='card-name'>
+                            <h2 className='product-name'>{product.name}</h2>
+                            <p className='product-price'>{product.price}ج</p>
+                            {/* <span className="material-symbols-outlined card-note" onClick={() => { setnoteArea(!noteArea); setproductid(product._id) }}>note_alt</span> */}
                           </div>
-                          <p>{product.description}</p>
-                        </div>
-                        <div className="price">
+                          <p className='card-discription'>{product.description}</p>
+                        {/* </div> */}
+                        {/* <div className="card-price">
                           <p>{product.price}ج</p>
-                          <div className="counter">
+                          <div className="card-counter">
                             <button className='symb' onClick={() => descrement(product._id)}>-</button>
                             <span className='num'>{product.quantity}</span>
                             <button className='symb' onClick={() => increment(product._id)}>+</button>
                           </div>
-                        </div>
-                        <div className='card-btn'>
-                        <button className='addtocart' onClick={() => { if (product.quantity > 0) { additemtocart(product._id, product.quantity) } }}>اضف الي طلباتي</button>
+                        </div> */}
+                        <div className='pos-btn'>
+                        {/* <button className='addtocart-btn' onClick={() => { if (product.quantity > 0) { additemtocart(product._id, product.quantity) } }}>اضف الي طلباتي</button> */}
 
-                          {/* {itemid.filter((i) => i == product._id).length > 0 && product.quantity > 0 ?
+                          {itemid.filter((i) => i == product._id).length > 0 && product.quantity > 0 ?
                             <button className='delfromcart' onClick={() => { deleteitems(product._id); setitemid(itemid.filter((i) => i !== product._id)) }}>احذف من الطلبات</button>
-                            : <button className='addtocart' onClick={() => { if (product.quantity > 0) { additemtocart(product._id, product.quantity) }; setitemid([...itemid, product._id]) }}>اضف الي طلباتي</button>} */}
+                            : <button className='addtocart' onClick={() => { if (product.quantity > 0) { additemtocart(product._id, product.quantity) }; setitemid([...itemid, product._id]) }}>اضف الي طلباتي</button>} 
                         </div>
                       </div>
                     </div>
