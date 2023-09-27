f17c1bc9a95b3649812116500ddf7a70baebe620
beshoynady <bishoin2017@gmail.com>
2023-09-27 17:24:35 +0300

    stock

--- server/router/StockMang.router.js
+++ server/router/StockMang.router.js
@@ -1,8 +1,7 @@
-const exports = require('exports');
-
+const express = require('express');
 const { addQuantity, takeOutQuantity } = require('../controllers/StockMang.controller');
 
-const router = express.router();
+const router = express.Router();
 
 router.route('/add').post(addQuantity)
 router.route('/takeOut').post(takeOutQuantity)
