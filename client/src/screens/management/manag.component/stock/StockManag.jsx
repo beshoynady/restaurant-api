86d7e46503c0f2253c1c90d6a0bedf2b063e8faf
beshoynady <bishoin2017@gmail.com>
2023-09-29 15:20:26 +0300

    aw

--- client/src/screens/management/manag.component/stock/StockManag.jsx
+++ client/src/screens/management/manag.component/stock/StockManag.jsx
@@ -228,7 +228,7 @@ const StockManag = () => {
                         </div>
                         <div className="form-group">
                           <label>الصنف</label>
-                          <select name="" id="" onChange={(e)=>{setitemId(e.target.value);setunit(e.target.unit) ;console.log(StockItems.filter(i=>i._id == e.target.value));console.log(e.target.value)}}>
+                          <select name="" id="" onChange={(e)=>{setitemId(e.target.value);setunit(e.target.unit) ;console.log(StockItems.filter(i=>i._id == e.target.value)[0].unit);console.log(e.target.value)}}>
                           <option >اختر الصنف</option>
                             {StockItems.map((item,i)=>{
                               return <option key={i} value={item._id}>{item.itemName}</option>
