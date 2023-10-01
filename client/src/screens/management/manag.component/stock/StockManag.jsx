e91755fa5cfcfd3448cc26112fdc24a4132af7ee
beshoynady <bishoin2017@gmail.com>
2023-10-01 23:08:26 +0300

    ssq

--- client/src/screens/management/manag.component/stock/StockManag.jsx
+++ client/src/screens/management/manag.component/stock/StockManag.jsx
@@ -54,11 +54,11 @@ const StockManag = () => {
       const changeItem = await axios.put(`https://restaurant-api-blush.vercel.app/api/stockitem/movement/${itemId}`,{newBalance,newcost,price})
       console.log(changeItem)
 
-    //   if(changeItem.status == 200){
-    //   const response = await axios.post('https://restaurant-api-blush.vercel.app/api/stockmanag/', { itemId, movement, Quantity, cost, unit,newBalance, oldBalance, price, actionBy, actionAt });
-    //   console.log(response.data);
-    //   getallStockaction()
-    // }
+      if(changeItem.status == 200){
+      const response = await axios.post('https://restaurant-api-blush.vercel.app/api/stockmanag/', { itemId, movement, Quantity, cost, unit,newBalance, oldBalance, price, actionBy, actionAt });
+      console.log(response.data);
+      getallStockaction()
+    }
     } catch (error) {
       console.log(error)
     }
