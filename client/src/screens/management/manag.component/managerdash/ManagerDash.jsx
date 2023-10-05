21b0957304a28e18d6a5223853c6820b528f64fa
beshoynady <bishoin2017@gmail.com>
2023-10-05 13:21:02 +0300

    aaa

--- client/src/screens/management/manag.component/managerdash/ManagerDash.jsx
+++ client/src/screens/management/manag.component/managerdash/ManagerDash.jsx
@@ -249,15 +249,15 @@ const ManagerDash = () => {
                     <i className='bx bx-plus'></i>
                   </div>
                   <ul className="task-list">
-                    {pending_payment.filter((order) => order.payment_status == 'انتظار' && order.isActive == false || order.help !=='لم يطلب').map((order, i) => {
+                    {pending_payment.filter((order) => order.payment_status == 'انتظار' && order.order_type=='داخلي'&& order.isActive == false || order.help !=='لم يطلب').map((order, i) => {
                       return (
                         <li className="completed" key={i}>
                           <div className="task-title">
-                            <p><i className='bx bx-check-circle'></i> {order.table != null ? usertitle(order.table) : usertitle(order.user)}</p>
-                            <p>{order.help?'يحتاج المساعدة': order.isActive == false? 'يحتاج الفاتورة': ''}</p>
-                            {order.help =='يطلب مساعدة'||order.help =='يطلب الفاتورة'?<button type="button" className="btn btn-primary" onClick={()=>sendwaiter(order._id)}>ارسال ويتر</button>:
+                            <p><i className='bx bx-check-circle'></i> {usertitle(order.table)}</p>
+                            <p>{order.help}</p>
+                            {order.help =='يطلب مساعدة' || order.help =='يطلب الفاتورة'?<button type="button" className="btn btn-primary" onClick={()=>sendwaiter(order._id)}>ارسال ويتر</button>:
                             <p>تم ارسال {usertitle(order.waiter)}</p>}
-                            <p>{order.table != null ? order.help : order.isActive == false? 'يحتاج الفاتورة': ''}</p>
+                            {/* <p>{order.table != null ? order.help : order.isActive == false? 'يحتاج الفاتورة': ''}</p> */}
                           </div>
                           <i className='bx bx-dots-vertical-rounded'></i>
                         </li>
