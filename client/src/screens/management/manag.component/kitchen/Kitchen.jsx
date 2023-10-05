1c3c33e25ff91c755c92d6eff7ccbb318e3a1b13
beshoynady <bishoin2017@gmail.com>
2023-10-05 18:39:41 +0300

    gg

--- client/src/screens/management/manag.component/kitchen/Kitchen.jsx
+++ client/src/screens/management/manag.component/kitchen/Kitchen.jsx
@@ -27,36 +27,43 @@ const Kitchen = () => {
   }
 
   const [waiters, setwaiters] = useState([])
-  const getAllWaiter = async()=>{
+  const getAllWaiter = async () => {
     const alluser = await axios.get('https://restaurant-api-blush.vercel.app/api/user')
-    const allwaiter =await alluser.data.filter((user)=>user.role === 'waiter')
-    // console.log(allwaiter)
+    console.log(alluser)
+    const allwaiter = await alluser.data.filter((user) => user.role === 'waiter')
+    console.log(allwaiter)
+    const waiterActive = await allwaiter.filter((waiter)=> waiter.isActive == true)
+    console.log(waiterActive)
     const listId = []
-    allwaiter.forEach((waiter)=>{
+    if(waiterActive){
+    waiterActive.forEach((waiter) => {
       listId.push(waiter._id)
-    })
-    // console.log(listId)
-    if(listId.length>0){
+    })}
+    console.log(listId)
+    if (listId.length > 0) {
       setwaiters(listId)
     }
   }
 
   // const [waiter, setwaiter] = useState()
-  const specifiedWaiter =()=>{
-    const lastwaiter = allorders? allorders[allorders.length-1].waiter:''
+  const specifiedWaiter = () => {
+    const ordertakewaiter = allorders.filter((order)=> order.waiter != null)
+    console.log(ordertakewaiter)
+    const lastwaiter = ordertakewaiter ? ordertakewaiter[allorders.length - 1].waiter : ''
     console.log(lastwaiter)
-    const indexoflastwaiter = waiters.indexOf(lastwaiter)
+
+    const indexoflastwaiter = lastwaiter? waiters.indexOf(lastwaiter): 0
 
     console.log(indexoflastwaiter)
-    console.log(indexoflastwaiter+1)
+    console.log(indexoflastwaiter + 1)
     console.log(waiters.length)
     console.log(waiters)
     // setwaiter(waiters[indexofwaiter+1])
-    if(waiters.length < indexoflastwaiter+1){
+    if (waiters.length == indexoflastwaiter + 1) {
       const waiter = waiters[0]
-      return waiter     
-    }else{
-      const waiter = waiters[indexoflastwaiter+1]
+      return waiter
+    } else {
+      const waiter = waiters[indexoflastwaiter + 1]
       return waiter
     }
   }
