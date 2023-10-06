7ce1bc681dabfa0bf073090b39194f6deeccb03b
beshoynady <bishoin2017@gmail.com>
2023-10-06 23:48:07 +0300

    ss

--- server/index.js
+++ server/index.js
@@ -50,13 +50,6 @@ app.use(cors({
   methods : ['GET', 'POST', 'PUT' , ' UPDATE', 'DELETE'],
   credentials: true 
 }));
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-  next();
-});
 
 app.use(cookieParser());
 app.use(express.json());
