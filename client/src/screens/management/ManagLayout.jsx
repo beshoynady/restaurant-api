6c8429ec51e23e88be51da497f68cdab090f00ac
beshoynady <bishoin2017@gmail.com>
2023-10-07 18:49:33 +0300

    aa

--- client/src/screens/management/ManagLayout.jsx
+++ client/src/screens/management/ManagLayout.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import './ManagLayout.css'
 import { detacontext } from '../../App'
 import { Navigate, Outlet } from 'react-router-dom';
@@ -10,41 +10,25 @@ import jwt_decode from "jwt-decode";
 
 const ManagLayout = () => {
 
-  const token = localStorage.getItem('token')
-    const decodetoken = jwt_decode(token)
-    const isadmin = decodetoken.userinfo.isAdmin
-    console.log(isadmin)
-    if (isadmin) {
-      return (
-        <div className='manag-screen'>
-          <SideBar />
-          <main className='manag_main'>
-            <NavBar />
-            <Outlet></Outlet>
-          </main>
-        </div>)
-    } else {
-      return <Navigate to={'/login'} />
-    }
-
-  // if (localStorage.getItem('token')) {
-  //   const tokenStorage = localStorage.getItem('token')
-  //   const decodetoken = jwt_decode(tokenStorage)
-  //    if(decodetoken.userinfo.isAdmin){
-  //     return(
-  //       <div className='manag-screen'>
-  //         <SideBar />
-  //         <main className='manag_main'>
-  //           <NavBar />
-  //           <Outlet></Outlet>
-  //         </main>
-  //       </div>)
-  //       }else{
-  //         return <Navigate to={'/login'} />
-  //       }
-  //   }else{
-  //         return <Navigate to={'/login'} />
-  //       }
-}
-
+    if (localStorage.getItem('token')) {
+      // console.log(localStorage.getItem('token'))
+      const tokenStorage = localStorage.getItem('token')
+      if (tokenStorage) {
+        const decodetoken = jwt_decode(tokenStorage)
+       if(decodetoken.userinfo.isAdmin){
+        return(
+          <div className='manag-screen'>
+            <SideBar />
+            <main className='manag_main'>
+              <NavBar />
+              <Outlet></Outlet>
+            </main>
+          </div>)
+          }
+        }
+      }else{
+            return <Navigate to={'/login'} />
+          }
+      }
+      
 export default ManagLayout
