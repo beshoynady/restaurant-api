3d8e847d5127d8a588a7c935d7ff4670224ad0a1
beshoynady <bishoin2017@gmail.com>
2023-10-07 19:51:48 +0300

    ManagLayout

--- client/src/screens/management/ManagLayout.jsx
+++ client/src/screens/management/ManagLayout.jsx
@@ -9,26 +9,33 @@ import jwt_decode from "jwt-decode";
 
 
 const ManagLayout = () => {
-
-    if (localStorage.getItem('token')) {
-      // console.log(localStorage.getItem('token'))
-      const tokenStorage = localStorage.getItem('token')
-      if (tokenStorage) {
-        const decodetoken = jwt_decode(tokenStorage)
-       if(decodetoken.userinfo.isAdmin){
-        return(
-          <div className='manag-screen'>
-            <SideBar />
-            <main className='manag_main'>
-              <NavBar />
-              <Outlet></Outlet>
-            </main>
-          </div>)
-          }
-        }
-      }else{
-            return <Navigate to={'/login'} />
-          }
+  return(
+    <div className='manag-screen'>
+      <SideBar />
+      <main className='manag_main'>
+        <NavBar />
+        <Outlet></Outlet>
+      </main>
+    </div>)
+    // if (localStorage.getItem('token')) {
+    //   // console.log(localStorage.getItem('token'))
+    //   const tokenStorage = localStorage.getItem('token')
+    //   if (tokenStorage) {
+    //     const decodetoken = jwt_decode(tokenStorage)
+    //    if(decodetoken.userinfo.isAdmin){
+    //     return(
+    //       <div className='manag-screen'>
+    //         <SideBar />
+    //         <main className='manag_main'>
+    //           <NavBar />
+    //           <Outlet></Outlet>
+    //         </main>
+    //       </div>)
+    //       }
+    //     }
+    //   }else{
+    //         return <Navigate to={'/login'} />
+    //       }
       }
       
 export default ManagLayout
